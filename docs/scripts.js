b06cc20d6976aec294ffc67a196a81e7154ba7b3
paul@remans.de <paul@remans.de>
2019-08-29 15:58:19 +0200

    textencoder

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,31 +1,33 @@
 "use strict";
 
-if ("undefined" == typeof TextEncoder) {
-  TextEncoder = function TextEncoder() {}, TextEncoder.prototype.encode = function (e) {
+if (void 0 === TextEncoder) {
+  var TextEncoder = function TextEncoder() {};
+
+  TextEncoder.prototype.encode = function (e) {
     "use strict";
 
-    for (var t = e.length, n = -1, o = "undefined" == typeof Uint8Array ? new Array(1.5 * t) : new Uint8Array(3 * t), r = 0, i = 0, c = 0; c !== t;) {
-      if (r = e.charCodeAt(c), c += 1, r >= 55296 && r <= 56319) {
+    for (var t = e.length, o = -1, r = "undefined" == typeof Uint8Array ? new Array(1.5 * t) : new Uint8Array(3 * t), n = 0, i = 0, c = 0; c !== t;) {
+      if (n = e.charCodeAt(c), c += 1, n >= 55296 && n <= 56319) {
         if (c === t) {
-          o[n += 1] = 239, o[n += 1] = 191, o[n += 1] = 189;
+          r[o += 1] = 239, r[o += 1] = 191, r[o += 1] = 189;
           break;
         }
 
         if (!((i = e.charCodeAt(c)) >= 56320 && i <= 57343)) {
-          o[n += 1] = 239, o[n += 1] = 191, o[n += 1] = 189;
+          r[o += 1] = 239, r[o += 1] = 191, r[o += 1] = 189;
           continue;
         }
 
-        if (c += 1, (r = 1024 * (r - 55296) + i - 56320 + 65536) > 65535) {
-          o[n += 1] = 240 | r >>> 18, o[n += 1] = 128 | r >>> 12 & 63, o[n += 1] = 128 | r >>> 6 & 63, o[n += 1] = 128 | 63 & r;
+        if (c += 1, (n = 1024 * (n - 55296) + i - 56320 + 65536) > 65535) {
+          r[o += 1] = 240 | n >>> 18, r[o += 1] = 128 | n >>> 12 & 63, r[o += 1] = 128 | n >>> 6 & 63, r[o += 1] = 128 | 63 & n;
           continue;
         }
       }
 
-      r <= 127 ? o[n += 1] = 0 | r : r <= 2047 ? (o[n += 1] = 192 | r >>> 6, o[n += 1] = 128 | 63 & r) : (o[n += 1] = 224 | r >>> 12, o[n += 1] = 128 | r >>> 6 & 63, o[n += 1] = 128 | 63 & r);
+      n <= 127 ? r[o += 1] = 0 | n : n <= 2047 ? (r[o += 1] = 192 | n >>> 6, r[o += 1] = 128 | 63 & n) : (r[o += 1] = 224 | n >>> 12, r[o += 1] = 128 | n >>> 6 & 63, r[o += 1] = 128 | 63 & n);
     }
 
-    return "undefined" != typeof Uint8Array ? o.subarray(0, n + 1) : (o.length = n + 1, o);
+    return "undefined" != typeof Uint8Array ? r.subarray(0, o + 1) : (r.length = o + 1, r);
   }, TextEncoder.prototype.toString = function () {
     return "[object TextEncoder]";
   };
